33bc5b06a102d5ea87afb10bf377c6e31ccd02d1
hunaredofficial <hunaredofficial@gmail.com>
2026-09-24 08:46:10 +0300

    new change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,7 +97,7 @@ export default function RootLayout({
         >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             themes={["light", "dark"]}
             disableTransitionOnChange
